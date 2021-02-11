e9c5fdf254e77b3b2fe0b8435d212ffbf869855d
Matthew Howard <mattalanhoward@gmail.com>
2021-02-11 11:36:37 +0100

    Work Container
    Add html, need to adjust image and slight change in text format

--- js/index.js
+++ js/index.js
@@ -38,6 +38,7 @@ showSlides();
 function showSlides() {
   let i;
   let slides = document.getElementsByClassName("slide");
+  let dots = document.getElementsByClassName("dot");
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
@@ -45,6 +46,10 @@ function showSlides() {
   if (slideIndex > slides.length) {
     slideIndex = 1;
   }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
   slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
   setTimeout(showSlides, 3000); // Change image every 2 seconds
 }
