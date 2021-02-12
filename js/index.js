01f926f407cd00c6e4a19e39042039dbb4badc14
Matthew Howard <mattalanhoward@gmail.com>
2021-02-12 12:55:39 +0100

    CurrentSlide() onclick
    Work on onclick currentslide bug.

--- js/index.js
+++ js/index.js
@@ -1,39 +1,10 @@
-// let slideIndex = 1;
-// showSlides(slideIndex);
-
-// // Next/previous controls
-// function plusSlides(n) {
-//   showSlides((slideIndex += n));
-// }
-
-// // Thumbnail image controls
-// function currentSlide(n) {
-//   showSlides((slideIndex = n));
-// }
-
-// function showSlides(n) {
-//   let i;
-//   let slides = document.getElementsByClassName("slide");
-//   let dots = document.getElementsByClassName("dot");
-//   if (n > slides.length) {
-//     slideIndex = 1;
-//   }
-//   if (n < 1) {
-//     slideIndex = slides.length;
-//   }
-//   for (i = 0; i < slides.length; i++) {
-//     slides[i].style.display = "none";
-//   }
-//   for (i = 0; i < dots.length; i++) {
-//     dots[i].className = dots[i].className.replace(" active", "");
-//   }
-//   slides[slideIndex - 1].style.display = "block";
-//   dots[slideIndex - 1].className += " active";
-//   setTimeout(showSlides, 3000); // Change image every 2 seconds
-// }
-
 let slideIndex = 0;
 showSlides();
+
+// Thumbnail image controls
+function currentSlide(n) {
+  slideIndex = n - 1;
+}
 
 function showSlides() {
   let i;
@@ -49,7 +20,8 @@ function showSlides() {
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
+
   slides[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " active";
-  setTimeout(showSlides, 3000); // Change image every 2 seconds
+  setTimeout(showSlides, 3000); // Change image every 3 seconds
 }
