16102fbb724b64043841220bf478598dc0ae7e78
Matthew Howard <mattalanhoward@gmail.com>
2021-02-13 21:25:42 +0100

    @Media Phones
    Add responsiveness for small devices.

--- js/index.js
+++ js/index.js
@@ -1,18 +1,19 @@
 let slideIndex = 0;
-showSlides();
 
 // Thumbnail image controls
-function currentSlide(n) {
+const currentSlide = (n) => {
   slideIndex = n - 1;
-}
+};
 
-function showSlides() {
+const showSlides = () => {
   let i;
   let slides = document.getElementsByClassName("slide");
   let dots = document.getElementsByClassName("dot");
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
+  // slides.forEach((slide) => (slide.style.display = "none"));
+
   slideIndex++;
   if (slideIndex > slides.length) {
     slideIndex = 1;
@@ -24,4 +25,6 @@ function showSlides() {
   slides[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " active";
   setTimeout(showSlides, 3000); // Change image every 3 seconds
-}
+};
+
+showSlides();
